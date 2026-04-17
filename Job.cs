d792c2a6c470b00a0a3acc9dc92e600ec6c03981
greatit-lab <gil2zum@gmail.com>
2026-04-17 20:10:22 +0900

    Update Job.cs --0417_20

--- Job.cs
+++ Job.cs
@@ -1,8 +1,17 @@
+// Job.cs
 using System;
 using System.Xml.Serialization;
 
 namespace SimpleScheduler
 {
+    public enum ScheduleType
+    {
+        Interval = 0,
+        Daily = 1,
+        Weekly = 2,
+        Monthly = 3
+    }
+
     public class Job
     {
         [XmlAttribute]
@@ -13,8 +22,14 @@ namespace SimpleScheduler
         public string Arguments { get; set; }
         public DateTime StartTime { get; set; } = DateTime.Now;
         public DateTime? EndTime { get; set; }
-        public int IntervalMinutes { get; set; } = 1;
         public bool IsEnabled { get; set; } = true;
+
+        // ▼ 추가된 스케줄 세부 옵션들
+        public ScheduleType RunType { get; set; } = ScheduleType.Interval;
+        public int IntervalMinutes { get; set; } = 1;
+        public DateTime DailyRunTime { get; set; } = DateTime.Today;
+        public DayOfWeek WeeklyRunDay { get; set; } = DayOfWeek.Monday;
+        public int MonthlyRunDay { get; set; } = 1;
 
         [XmlIgnore]
         public DateTime NextRunTime { get; set; }
